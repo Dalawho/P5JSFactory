3b2ce5abfec131e861ac3f77439dcf15c3b87f47
Dalawho <l_lercher@hotmail.com>
2023-04-08 21:09:10 +0200

    first public commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## P5Factory
 A simple Factory contract setup to make deploying P5JS projects using scripty easy and cheap. 
 
+2 Variables are available in the p5js script: 
+- randomNr: A uint96 random number generated at mint
+- tokenId: TokenNr of the NFT
+
--- src/P5Logic.sol
+++ src/P5Logic.sol
@@ -8,9 +8,16 @@ import {MerkleProofUpgradeable} from "openzeppelin-upgradeable/utils/cryptograph
 import {DefaultOperatorFiltererUpgradeable} from "operator-filter-registry/upgradeable/DefaultOperatorFiltererUpgradeable.sol";
 import {Initializable} from "openzeppelin-upgradeable/proxy/utils/Initializable.sol";
 import { SSTORE2 } from "solmate/utils/SSTORE2.sol";
-import { P5Render} from "./P5Render.sol";
+import {IScriptyBuilder, WrappedScriptRequest} from "scripty/IScriptyBuilder.sol";
+import {Base64} from "solady/utils/Base64.sol";
+import {StringsUpgradeable as Strings} from "openzeppelin-upgradeable/utils/StringsUpgradeable.sol";
+
 
 contract P5Logic is Initializable, ERC721, OwnableUpgradeable, DefaultOperatorFiltererUpgradeable{
+
+    address public constant ethfsFileStorageAddress = 0xFc7453dA7bF4d0c739C1c53da57b3636dAb0e11e;
+    address public constant scriptyStorageAddress = 0x096451F43800f207FC32B4FF86F286EdaF736eE3;
+    address public constant scriptyBuilderAddress  = 0x16b727a2Fc9322C724F4Bc562910c99a5edA5084;
 
     bytes32 public ALRoot;
     uint256 public mintStatus; 
@@ -19,7 +26,6 @@ contract P5Logic is Initializable, ERC721, OwnableUpgradeable, DefaultOperatorFi
     uint256 public maxPerWallet;
     uint256 public pricePerNFT;
     address public script;
-    P5Render public render;
 
     error PublicMintNotStarted();
     error PayMintPrice();
@@ -35,7 +41,7 @@ contract P5Logic is Initializable, ERC721, OwnableUpgradeable, DefaultOperatorFi
         _disableInitializers();
     }
 
-    function initialize(string memory _name, address _render, string memory _symbol, uint256 _maxSupply, uint256 _maxPerWallet, uint256 _pricePerNFT, bytes memory _script) initializer public { 
+    function initialize(string memory _name, string memory _symbol, uint256 _maxSupply, uint256 _maxPerWallet, uint256 _pricePerNFT, bytes memory _script) initializer public { 
         __ERC721_init(_name, _symbol, 1);
         __Ownable_init();
         __DefaultOperatorFilterer_init();
@@ -44,7 +50,6 @@ contract P5Logic is Initializable, ERC721, OwnableUpgradeable, DefaultOperatorFi
         maxPerWallet = _maxPerWallet;
         pricePerNFT = _pricePerNFT;
         script = SSTORE2.write(_script);
-        render = P5Render(_render);
     }
 
     /// Admin setters
@@ -100,18 +105,53 @@ contract P5Logic is Initializable, ERC721, OwnableUpgradeable, DefaultOperatorFi
     //TokenURI
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
         if(tokenId == 0 || tokenId > tokenIndex) revert();
-        return render.tokenURI(tokenId, _ownerOf[tokenId].random, SSTORE2.read(script));
+        WrappedScriptRequest[] memory requests = new WrappedScriptRequest[](5);
+        requests[0].name = "scriptyBase";
+        requests[0].wrapType = 0; // <script>[script]</script>
+        requests[0].contractAddress = scriptyStorageAddress;
+
+        requests[1].name = "p5-v1.5.0.min.js.gz";
+        requests[1].wrapType = 2; // <script type="text/javascript+gzip" src="data:text/javascript;base64,[script]"></script>
+        requests[1].contractAddress = ethfsFileStorageAddress;
+
+        requests[2].name = "gunzipScripts-0.0.1.js";
+        requests[2].wrapType = 1; // <script src="data:text/javascript;base64,[script]"></script>
+        requests[2].contractAddress = ethfsFileStorageAddress;
+
+        requests[3].wrapType = 0; // <script>[script]</script>
+        requests[3].scriptContent = abi.encodePacked("var randomNr = ", Strings.toString(_ownerOf[tokenId].random), "; var tokenId = ", Strings.toString(tokenId), ";");
+
+        requests[4].wrapType = 0; // <script>[script]</script>
+        requests[4].scriptContent = SSTORE2.read(script);
+
+        // For lazy devs that dont want to mess around with buffersize off-chain
+        // calculate it here
+        IScriptyBuilder scriptyBuilder = IScriptyBuilder(scriptyBuilderAddress);
+
+        uint256 bufferSize = scriptyBuilder.getBufferSizeForHTMLWrapped(requests);
+
+        bytes memory base64EncodedHTMLDataURI = IScriptyBuilder(scriptyBuilderAddress)
+            .getEncodedHTMLWrapped(requests, bufferSize);
+
+        bytes memory metadata = abi.encodePacked(
+            '{"name":"p5.js Clone Example", "description":"A cloned contract for cheap p5js deployments.","animation_url":"',
+            base64EncodedHTMLDataURI,
+            '"}'
+        );
+
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(metadata)
+                )
+            );    
     }
 
     //
     function withdraw() external onlyOwner {
         if(mintReciever == address(0)) revert SplitterNotSet();
         payable(mintReciever).transfer(address(this).balance); //should I change this? 
-    }
-
-    function contractURI() public view returns (string memory) {
-        //return getContractInfo();
-        //write something here
     }
 
     //////////////////////// Operatorfilter overrides ////////////////////////
